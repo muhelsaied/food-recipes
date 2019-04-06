0a66654fcef5c81b8d76dea42f5f25a054d28edd
MUHAMED ELSAIED <muhahmed2099@gmail.com>
2019-04-06 15:52:00 +0200

    rcipeList -ajax

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -4,12 +4,32 @@ import RecipesList from '../compenets/search/recipesList'
 import { recipeData } from '../data/tempList'
 
 class Recipes extends Component {
-    // constructor(props){
-    //     super(props);
-    // }
+    constructor(props){
+        super(props);
+        this.getRecipes = this.getRecipes.bind(this);
+    }
     state={
       recipes : recipeData,
-      search: ''
+      search: '',
+      url:`https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`
+    };
+
+    async getRecipes(){
+        try{
+            const data = await fetch(this.state.url);
+            //run with json
+            const jsonData = await data.json();
+            // console.log(jsonData)
+            this.setState({
+                recipes: jsonData.recipes
+            })
+        }
+        catch(error){
+            console(error)
+        }
+    }
+    componentDidMount(){
+        this.getRecipes();
     }
 // change input in search 
 handlechange = e =>{
